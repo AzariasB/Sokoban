bf928d522afe26d2ab1be8e4562f25a9b6a40504
Azarias Boutin <azarias.boutin@gmail.com>
2018-03-21 11:23:53 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <fstream>
+#include "Map.h"
+
+Map parseFile(const std::string &mapName)
+{
+    std::ifstream mapF;
+    mapF.open(mapName);
+    if(!mapF.is_open()){
+        std::cerr << "File not found " << std::endl;
+        return Map();//;//empty map
+    }
+
+    int width = 0;
+    int height = 0;
+    mapF >> width;
+    mapF.ignore();// comma
+    mapF >> height;
+    mapF.ignore();// \n
+
+    std::cout << "Width = " << width << " - height = " << height << std::endl;
+
+    Map mMap;//(width, height);
+    for(int y = 0; y < height; ++y){
+        for(int x = 0; x < width; ++x){
+            int value;
+            mapF >> value;
+            mapF.ignore();
+            std::cout << value << " ";
+            //CellsTypes ct = Map::toCellType(value);
+            //mMap.setValue(x,y,ct);
+        }
+        std::cout << std::endl;
+    }
+    return mMap;
+}
+
+int main() {
+    parseFile("../data/soko1.txt");
+    return 0;
+}
