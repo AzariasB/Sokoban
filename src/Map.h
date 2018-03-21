bcd4d372a0a08a5e2fb4053fc99660576d2abebc
Raphaël Haltz <lepointgeek@gmail.com>
2018-03-21 11:19:42 +0100

    Add Map methods

--- src/Map.h
+++ src/Map.h
@@ -5,6 +5,10 @@
 #ifndef SOKOBAN_MAP_H
 #define SOKOBAN_MAP_H
 
+#include <vector>
+
+using namespace std;
+
 enum CellsTypes {
     Empty = 0,
     Wall,
@@ -15,7 +19,22 @@ enum CellsTypes {
 };
 
 class Map {
+public:
+    Map();
+    Map(int width, int height);
 
+    void SetWidth(int width);
+    void SetHeight(int height);
+    int GetWidth(void);
+    int GetHeight(void);
+
+    CellsTypes GetTypeFromInt(int input);
+    void SetXY(int x, int y, CellsTypes type);
+    CellsTypes GetXY(int x, int y);
+private:
+    int width;
+    int height;
+    vector<vector<CellsTypes>> map;
 };
 
 
